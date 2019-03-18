4d68dd4a7c976b636bc72eaf747f850290a6c6af
Anwesh43 <anweshthecool0@gmail.com>
2019-03-19 01:23:43 +0530

    adding libraries and constants

--- containercirclesview/src/main/java/com/anwesh/uiprojects/containercirclesview/ContainerCirclesView.kt
+++ containercirclesview/src/main/java/com/anwesh/uiprojects/containercirclesview/ContainerCirclesView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.containercirclesview
 /**
  * Created by anweshmishra on 19/03/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val balls : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
